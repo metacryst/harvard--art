5db4d41b79a55ccdfee5242e2402e9c7e72a6473
metacryst <samrussell99@gmail.com>
2020-04-30 13:31:12 -0500

    Displaying paintings

--- src/components/Art.js
+++ src/components/Art.js
@@ -4,20 +4,20 @@ class Art extends Component {
     render() {
         const arts = this.props.artData
         for(let i=0; i< arts.length; i++) {
-            if(this.props.match.params.name === arts[i].name) {
+            if(this.props.match.params.title === arts[i].title) {
                 let art = arts[i];
                 return (
                   <div>
-                    <img src={art.image} className="singleImage" alt={art.name}></img>
+                    <img src={art.primaryimageurl} className="singleImage" alt={art.title}></img>
                       <div id="text">
-                        <h1>{art.name}</h1>
-                        <h2>{`(${art.genus})`}</h2>
+                        <h1>{art.title}</h1>
+                        <h2>{`(${art.people[0].name})`}</h2>
                         <div className="conservationStatus">
-                          <h3>Conservation Status</h3>
-                          <p className="conservationStatusText">{art.conservationStatus}</p>
+                          <h3>Medium</h3>
+                          <p className="conservationStatusText">{art.medium}</p>
                         </div>
                         <div>
-                          <a className="readMore"href={art.homepage}>Read More</a>
+                          <a className="readMore"href={art.url}>Read More</a>
                         </div>
                       </div>
                   </div>
--- src/components/Arts.js
+++ src/components/Arts.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-class Birds extends Component {
-  
-    render() {
-    let list = this.props.artData.map(item => {
-      return (
-        <div className="Arts" key={item.name}>
-            <Link to={"/Art/"+ item.name}><img src={item.image} alt={item.name}></img></Link>
-        </div>
-      );
+class Arts extends Component {
+  render() {
+    let list = this.props.artData.map((item) => {
+      if (item.primaryimageurl != null) {
+        return (
+          <div className="Arts" key={item.title}>
+            <Link to={"/Art/" + item.title}>
+              <img
+                className="galleryPiece"
+                src={item.primaryimageurl}
+                alt={item.title}
+              ></img>
+            </Link>
+          </div>
+        );
+      }
     });
-    return (
-        <div>
-          {list}
-        </div>
-    )
+    return <div>{list}</div>;
   }
 }
 
-export default Birds;
+export default Arts;
